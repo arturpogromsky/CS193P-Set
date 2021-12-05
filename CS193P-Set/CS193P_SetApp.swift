4b6179aa38a23dd5cdc07fb18797caaf928e1ef6
arturpogromsky <ar.pogromsky@gmail.com>
2021-12-05 16:49:06 +0300

    Initial Commit

---
+++ CS193P-Set/CS193P_SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_SetApp.swift
+//  CS193P-Set
+//
+//  Created by Артур Погромский on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
